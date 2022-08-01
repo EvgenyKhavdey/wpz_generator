c49244f5cfedd66a8742d4e117e66694a418e825
Evgeny Khavdey <khavdey61@yandex.ru>
2022-08-01 19:38:37 +0300

    Initial commit

---
+++ src/main/java/ru/wpz/wpz_generator/WpzGeneratorApplication.java
@@ -0,0 +1,13 @@
+package ru.wpz.wpz_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpzGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WpzGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/wpz/wpz_generator/WpzGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wpz.wpz_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpzGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
