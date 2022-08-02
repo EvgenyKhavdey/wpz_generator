7f5199893aff6ca8f8e0716c65f9f7dc41a5b88c
Evgeny Khavdey <khavdey61@yandex.ru>
2022-08-02 19:46:46 +0300

    добавлен ExecutorService

--- src/main/java/ru/wpz/wpz_generator/Generator.java
+++ src/main/java/ru/wpz/wpz_generator/Generator.java
@@ -4,9 +4,7 @@ import ru.wpz.wpz_generator.domain.Message;
 import ru.wpz.wpz_generator.kafka.KafkaTopic;
 import ru.wpz.wpz_generator.kafka.core.KafkaHelper;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.*;
 
 public class Generator {
 
@@ -16,29 +14,27 @@ public class Generator {
         this.kafkaHelper = kafkaHelper;
     }
 
-    Map<Integer, Integer> mapGenerator = new HashMap<>()
+    ConcurrentHashMap<Integer, Integer> mapGenerator = new ConcurrentHashMap<>()
     {{ put(333, 0); put(334, 0); put(335, 0); put(336, 0); put(337, 0); put(338, 0); put(339, 0); put(340, 0); put(341, 0); put(342, 0); put(343, 0);
         put(344, 0); put(345, 0); put(346, 0); put(347, 0); put(348, 0); put(349, 0); put(350, 0); put(351, 0); put(352, 0); put(353, 0); put(354, 0);
         put(355, 0); put(356, 0); put(357, 0); put(358, 0); put(359, 0); put(360, 0); put(361, 0); put(362, 0); put(363, 0); put(364, 0); put(365, 0);
         put(366, 0); put(367, 0); put(368, 0); put(369, 0); put(370, 0); put(371, 0); put(372, 0); put(373, 0); put(374, 0); put(375, 0); put(376, 0);
         put(377, 0); put(378, 0); put(379, 0); put(380, 0); put(381, 0); put(382, 0); put(383, 0); put(384, 0); put(385, 0); put(386, 0); put(387, 0);}};
 
-    public void methodGenerator(){
-        Thread t1 = new Thread(() -> {
-            while (true) {
-                try {
-                    creatMessage();
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+    public void methodGenerator() {
+        try {
+            ExecutorService executorService = Executors.newFixedThreadPool(5);
+            for (int i = 0; i < 10; i++) {
+                executorService.execute(new RunnableImpl());
             }
-        });
-        t1.setDaemon(true);
-        t1.start();
+            executorService.shutdown();
+            executorService.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void creatMessage() throws Exception {
+    public void creatMessage() throws Exception {
         int i = 0;
         Message message = new Message();
 
@@ -58,5 +54,19 @@ public class Generator {
         message.setDevId(devId);
         message.setStatus(mapGenerator.get(devId));
         kafkaHelper.sendTo(KafkaTopic.TEST, message.toString());
+
+    }
+
+    class RunnableImpl implements Runnable {
+        @Override
+        public void run() {
+            try {
+                creatMessage();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
+
